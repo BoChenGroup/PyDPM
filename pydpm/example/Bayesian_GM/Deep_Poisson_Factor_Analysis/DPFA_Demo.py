7041956448975a0740bf3d3295f5092eb573d70b
xinyangATK <94479131+xinyangATK@users.noreply.github.com>
2022-12-04 22:55:10 +0800

    Add files via upload

--- pydpm/example/Bayesian_GM/Deep_Poisson_Factor_Analysis/DPFA_Demo.py
+++ pydpm/example/Bayesian_GM/Deep_Poisson_Factor_Analysis/DPFA_Demo.py
@@ -27,9 +27,9 @@ test_dataset = datasets.MNIST(root='../../dataset/mnist/', train=False, download
 
 # transform dataset and reshape the dataset into [batch_size, feature_num]
 train_data = tensor_transforms(train_dataset.data, transform)
-train_data = train_data.permute(1, 0, 2).reshape(len(train_dataset), -1)# len(train_dataset, 28*28)
+train_data = train_data.permute([1, 2, 0]).reshape(len(train_dataset), -1)  # len(train_dataset, 28*28)
 test_data = tensor_transforms(test_dataset.data, transform)
-test_data = test_data.permute(1, 0, 2).reshape(len(test_dataset), -1)
+test_data = test_data.permute([1, 2, 0]).reshape(len(test_dataset), -1)
 train_label = train_dataset.train_labels
 test_label = test_dataset.test_labels
 
@@ -40,12 +40,12 @@ train_label = train_label.numpy()[:999]
 test_label = test_label.numpy()[:999]
 
 # create the model and deploy it on gpu or cpu
-model = DPFA([128, 64, 32], 'gpu')  # topics of 3 layers
+model = DPFA([128, 64, 32], 'gpu')  # topics of each layers
 model.initial(train_data)  # use the shape of train_data to initialize the params of model
 burnin, collection = 100, 80
-train_local_params = model.train(burnin, collection, train_data)
-train_local_params = model.test(burnin, collection, train_data)
-test_local_params = model.test(burnin, collection, test_data)
+train_local_params = model.train(train_data, burnin=burnin, collection=collection)
+train_local_params = model.test(train_data, burnin=burnin, collection=collection)
+test_local_params = model.test(test_data, burnin=burnin, collection=collection)
 
 # evaluate the model with classification accuracy
 # the demo accuracy can achieve 0.9099
