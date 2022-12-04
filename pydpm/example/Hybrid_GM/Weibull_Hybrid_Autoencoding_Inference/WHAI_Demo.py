620c36bd2871d6c8b287b47198df581a3b8ff535
xinyangATK <94479131+xinyangATK@users.noreply.github.com>
2022-12-04 23:00:27 +0800

    Add files via upload

--- pydpm/example/Hybrid_GM/Weibull_Hybrid_Autoencoding_Inference/WHAI_Demo.py
+++ pydpm/example/Hybrid_GM/Weibull_Hybrid_Autoencoding_Inference/WHAI_Demo.py
@@ -15,17 +15,16 @@ import numpy as np
 import argparse
 import scipy.io as sio
 from sklearn.cluster import k_means
-
+from nltk.corpus import stopwords
 from pydpm.model import WHAI
 from pydpm.utils import *
 from pydpm.metric import *
-from pydpm.dataloader.text_data import Text_Processer
-
+from pydpm.dataloader.text_data import Text_Processer, build_vocab_from_iterator
+from sklearn.metrics.cluster import normalized_mutual_info_score
 import torch
 from torch.utils.data import DataLoader
 
 from torchtext.data.utils import get_tokenizer
-from torchtext.vocab import build_vocab_from_iterator, GloVe
 from torchtext.datasets import AG_NEWS
 
 parser = argparse.ArgumentParser()
@@ -38,7 +37,7 @@ parser.add_argument("--dataset", type=str, default='AG_NEWS', help="the name of 
 parser.add_argument("--dataset_path", type=str, default='../../dataset', help="the file path of dataset")
 
 # network settings
-parser.add_argument("--voc_size", type=int, default=5000, help="the length of vocabulary")
+parser.add_argument("--voc_size", type=int, default=20000, help="the length of vocabulary")
 parser.add_argument("--z_dims", type=list, default=[128, 64, 32], help="the list of z dimension")
 parser.add_argument("--hid_dims", type=list, default=[200, 200, 200], help="the list of hidden dimension")
 
@@ -46,7 +45,7 @@ parser.add_argument("--hid_dims", type=list, default=[200, 200, 200], help="the 
 parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
 
 # training
-parser.add_argument("--num_epochs", type=int, default=50, help="number of epochs of training")
+parser.add_argument("--num_epochs", type=int, default=1000, help="number of epochs of training")
 parser.add_argument("--MBratio", type=int, default=50, help="number of epochs of training")
 parser.add_argument("--batch_size", type=int, default=256, help="size of the batches")
 
@@ -58,23 +57,23 @@ train_iter, test_iter = AG_NEWS('../../dataset/', split=('train', 'test'))
 tokenizer = get_tokenizer("basic_english")
 
 # build vocabulary
-vocab = build_vocab_from_iterator(map(lambda x: tokenizer(x[1]), train_iter), specials=['<unk>', '<pad>', '<bos>', '<eos>'], special_first=True, max_tokens=args.voc_size)
+stop_words = list(stopwords.words('english'))
+vocab = build_vocab_from_iterator(map(lambda x: tokenizer(x[1]), train_iter), specials=['<unk>', '<pad>', '<bos>', '<eos>'], special_first=True, stop_words=stop_words, max_tokens=args.voc_size)
 vocab.set_default_index(vocab['<unk>'])
 text_processer = Text_Processer(tokenizer=tokenizer, vocab=vocab)
 
 
 # Get train/test label and data_file(tokens) from data_iter and convert them into clean file
-stop_words = ['<unk>']
-train_files, train_labels = text_processer.file_from_iter(train_iter, tokenizer=tokenizer, stop_words=stop_words)
-test_files, test_labels = text_processer.file_from_iter(test_iter, tokenizer=tokenizer, stop_words=stop_words)
+train_files, train_labels = text_processer.file_from_iter(train_iter, tokenizer=tokenizer)
+test_files, test_labels = text_processer.file_from_iter(test_iter, tokenizer=tokenizer)
 
 # Take part of dataset for convenience
-train_idxs = np.arange(2000)
+train_idxs = np.arange(7000)
 np.random.shuffle(train_idxs)
 train_files = [train_files[i] for i in train_idxs]
 train_labels = [train_labels[i] for i in train_idxs]
 
-test_idxs = np.arange(2000)
+test_idxs = np.arange(3000)
 np.random.shuffle(test_idxs)
 test_files = [test_files[i] for i in test_idxs]
 test_labels = [test_labels[i] for i in test_idxs]
@@ -83,46 +82,27 @@ test_labels = [test_labels[i] for i in test_idxs]
 train_bows, train_labels = text_processer.bow_from_file(train_files, train_labels)
 test_bows, test_labels = text_processer.bow_from_file(test_files, test_labels)
 
-train_loader = DataLoader([train_data for train_data in zip(train_bows, train_labels)], batch_size=2, shuffle=False, num_workers=1, drop_last=True)
-test_loader = DataLoader([test_data for test_data in zip(test_bows, test_labels)], batch_size=2, shuffle=False, num_workers=1, drop_last=True)
+train_loader = DataLoader([train_data for train_data in zip(train_bows, train_labels)], batch_size=256, shuffle=False, num_workers=4, drop_last=True)
+test_loader = DataLoader([test_data for test_data in zip(test_bows, test_labels)], batch_size=256, shuffle=False, num_workers=4, drop_last=True)
 
-
+cls_num = len(np.unique(train_labels + test_labels))
 model = WHAI(in_dim=args.voc_size, z_dims=args.z_dims, hid_dims=args.hid_dims, device=args.device)
 model_opt = torch.optim.Adam(model.parameters())
 
 for epoch in range(args.num_epochs):
     train_local_params = model.train_one_epoch(dataloader=train_loader, optim=model_opt, epoch_index=epoch, args=args, is_train=True)
+    if (epoch+1) % 20 == 0:
+        test_local_params = model.test_one_epoch(dataloader=test_loader)
+        # calculate PPL
+        x_hat = np.matmul(test_local_params.Theta[0], np.transpose(model.global_params.Phi[0]))
+        ppl = Perplexity(test_local_params.data, x_hat)
+
+        # calculate NMI with train_local_params
+        test_theta_norm = standardization(test_local_params.Theta[0])
+        tmp = k_means(test_theta_norm, cls_num)  # N*K
+        predict_label = tmp[1] + 1 # Some label start with '1' not '0', there should be 'tmp[1] + 1'
+        MI = NMI(test_local_params.label, predict_label)
 
 
 
-#
-# model = model.cuda()
-# model_opt = torch.optim.Adam(model.parameters())
-#
-# # Training
-#
-# for epoch in range(n_epochs):
-#     train_local_params = model.train_one_epoch(model_opt=model_opt, dataloader=train_loader, epoch=epoch, update_phi=True)
-#     if epoch % 20 == 0:
-#         model.save_phi(phi_path=phi_out, epoch=epoch)
-#         test_theta, test_label, full_test_data = model.test_one_epoch(dataloader=test_loader)
-#         # calculate PPL
-#         x_hat = np.matmul(test_theta, np.transpose(model.global_params.Phi[0]))
-#         ppl = Perplexity(np.transpose(full_test_data), np.transpose(x_hat))
-#         # calculate NMI with train_local_params
-#         test_data_norm = standardization(train_local_params.theta)
-#         tmp = k_means(test_data_norm, cls_num)  # N*K
-#         predict_label = tmp[1] + 1 # Some label start with '1' not '0', there should be 'tmp[1] + 1'
-#         MI = NMI(train_local_params.label, predict_label)
-#
-# # save the model after training
-# model.save()
-#
-# # load the model and test
-# whai = WHAI(K=K, H=H, V=len(voc), device='cuda:0')
-#
-# whai.load('../save_models/WHAI.pth', '../save_models/WHAI.npy')
-# test_theta, test_label, full_test_data = whai.test_one_epoch(dataloader=test_loader)
-# x_hat = np.matmul(test_theta, np.transpose(whai.global_params.Phi[0]))
-# ppl = Perplexity(np.transpose(full_test_data), np.transpose(x_hat))
 
