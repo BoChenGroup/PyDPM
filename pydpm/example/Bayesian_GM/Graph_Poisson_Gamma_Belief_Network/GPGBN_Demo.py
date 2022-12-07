961f3505185d321533b0c91578d3420536137758
xinyangATK <94479131+xinyangATK@users.noreply.github.com>
2022-12-07 11:22:25 +0800

    update GPGBN_Demo.py

--- pydpm/example/Bayesian_GM/Graph_Poisson_Gamma_Belief_Network/GPGBN_Demo.py
+++ pydpm/example/Bayesian_GM/Graph_Poisson_Gamma_Belief_Network/GPGBN_Demo.py
@@ -18,7 +18,7 @@ import scipy.io as sio
 from pydpm.metric import ACC
 from pydpm.model import GPGBN
 from pydpm.dataloader.image_data import tensor_transforms
-from pydpm.dataloader.graph_data import graph_from_data, graph_from_edges
+from pydpm.dataloader.graph_data import Graph_Processer
 from pydpm.utils import cosine_simlarity
 
 from torchvision import datasets, transforms
@@ -54,7 +54,8 @@ train_data = np.array(np.ceil(train_data[:999, :].T.numpy() * 5), order='C')
 train_label = train_label.numpy()[:999]
 
 # construct the adjacency matrix
-graph = graph_from_data(train_data.T, 0.5, binary=False)
+graph_processer = Graph_Processer()
+graph = graph_processer.graph_from_node_feature(train_data.T, 0.5, binary=False)
 
 
 
