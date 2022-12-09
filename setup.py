cb13c7742e38ef59412addc78f11f5dc1a5882d7
WeiZhao_XDU <1353253303@qq.com>
2022-12-09 16:58:13 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='pydpm',
-    version='2.0.0',
+    version='4.0.1',
     description='A python library focuses on constructing deep probabilistic models on GPU.',
     py_modules=['pydpm'],
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    author='Chaojie Wang, Wei Zhao, Jiawen Wu',
+    author='Chaojie Wang, Wei Zhao, Xinyang Liu, Jiawen Wu',
     author_email='xd_silly@163.com',
     maintainer='BoChenGroup',
     maintainer_email='13279389260@163.com',
@@ -22,7 +22,7 @@ setup(
     include_package_data=True,  # include all files
     platforms=["Windows", "Linux"],
     url='https://github.com/BoChenGroup/Pydpm',
-    requires=['numpy', 'scipy', 'sklearn', 'pycuda'],
+    requires=['numpy', 'scipy', 'sklearn', 'PyTorch', 'ctypes', 'subprocess', ],
     classifiers=[
         'Environment :: GPU :: NVIDIA CUDA',
         'Intended Audience :: Developers',
